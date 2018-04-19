ffb001426c513acef3df6867fec251424a2dfb0f
Electrik Youth <electrikyouthx@hotmail.com>
2018-04-19 15:23:32 -0700

    modified/added

--- gcc_termux/termux-packages/experimental-packages/gcc-6/libgfortran.subpackage.sh
+++
@@ -1,3 +0,0 @@
-TERMUX_SUBPKG_INCLUDE="lib/gcc/$TERMUX_HOST_PLATFORM/${TERMUX_PKG_VERSION}/libgfortran.so lib/gcc/$TERMUX_HOST_PLATFORM/${TERMUX_PKG_VERSION}/libgfortran.so.3 lib/gcc/$TERMUX_HOST_PLATFORM/${TERMUX_PKG_VERSION}/libgfortran.so.3.0.0 lib/libgfortran.so lib/libgfortran.so.3 lib/libgfortran.so.3.0.0"
-TERMUX_SUBPKG_DESCRIPTION="dynamic library for fortran"
-#TERMUX_SUBPKG_DEPENDS=""
